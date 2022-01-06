61e903e6f1636e50d41ff367806ddc822fdf2656
arash <arashsafari86@gmail.com>
2022-01-06 12:52:55 +0330

    d

--- src/pages/app/pages/task/task.js
+++ src/pages/app/pages/task/task.js
@@ -79,12 +79,14 @@ const TaskPage = () => {
       const ai_predicteds = task["items"][1]["meta-label"]["ai"];
       let new_tags = [];
       for (let i = 0; i < ai_predicteds.length; i++) {
-        console.log(i);
         let tag = findClassByIndex(classes, ai_predicteds[i].index);
         if (!(tag in preferedClasses) && new_tags.length < 5) {
           new_tags.push(tag);
         }
       }
+      console.log(new_tags);
+      console.log("preferedclasses", preferedClasses);
+      console.log("all tags", preferedClasses.concat(new_tags));
       setPreferedClasses(preferedClasses.concat(new_tags));
     }
   };
